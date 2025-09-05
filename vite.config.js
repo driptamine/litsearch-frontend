77bbb17bc9d57a47d9ccc550e9ae2dd9d3e12873
Flipworld <talga.tawit@gmail.com>
2025-09-05 11:49:10 +0500

    Added Github Pages: gh-vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc';
 import { visualizer } from 'rollup-plugin-visualizer'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 
 import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
@@ -24,6 +25,7 @@ export default defineConfig({
         ]
       }
     }),
+    ghPages(),
     visualizer({
       open: true, // Automatically opens the visualizer in your browser
     }),
