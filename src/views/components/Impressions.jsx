04d22c1d5df8e000a4f4756b0c92fd5a6d90572a
Flipworld <talga.tawit@gmail.com>
2026-06-24 10:39:08 +0500

    add pop animation to impressions count on update

--- src/views/components/Impressions.jsx
+++ src/views/components/Impressions.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { styled } from '@linaria/react';
+import { styled, keyframes } from '@linaria/react';
 import { FaEye } from 'react-icons/fa';
 
 const Impressions = ({ count = 0 }) => (
   <ImpressionsWrap>
     <StyledFaEye />
-    <span>{count}</span>
+    <Count key={count}>{count}</Count>
   </ImpressionsWrap>
 );
+
+const pop = keyframes`
+  0% { transform: scale(1); }
+  50% { transform: scale(1.3); color: #0f6; }
+  100% { transform: scale(1); }
+`;
 
 const ImpressionsWrap = styled.div`
   display: flex;
@@ -19,6 +25,10 @@ const ImpressionsWrap = styled.div`
 
 const StyledFaEye = styled(FaEye)`
   font-size: 14px;
+`;
+
+const Count = styled.span`
+  animation: ${pop} 0.35s ease;
 `;
 
 export default Impressions;
