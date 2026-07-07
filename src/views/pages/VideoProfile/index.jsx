fe133b445ccb4dd8e4854a75dba1ffcf56abaad4
Flipworld <talga.tawit@gmail.com>
2026-07-07 05:32:22 +0500

    wire VideoProfile to new /videos/:id endpoint

--- src/views/pages/VideoProfile/index.jsx
+++ src/views/pages/VideoProfile/index.jsx
@@ -1,165 +1,88 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { styled } from '@linaria/react';
-
-// MATERIAL DONE
-// import { Typography } from '@mui/material';
-import { StyledTypography } from 'views/styledComponents';
-import Profile from 'views/components/Profile';
+import { LITLOOP_API_URL } from 'core/constants/urls';
+import { authHeader } from 'core/api/rest-helper';
 import VideoIntroduction from './VideoIntroduction';
-import CustomPlayerV4 from 'views/components/video-player/web/CustomPlayerV4';
 import RecommendedVideo from 'views/components/video-player/web/RecommendedVideo';
 import CommentV4 from 'views/pages/AlbumProfile/CommentV4';
-import App from 'views/pages/AlbumProfile/Tree/App';
-import RedditApp from 'views/pages/AlbumProfile/Tree/RedditApp';
-
-// import MovieImageGridList from './MovieImageGridList';
-// import MovieVideoList from './MovieVideoList';
-// import MovieCastGridList from './MovieCastGridList';
-// import Recommendations from './Recommendations';
-
-// CORE
-import { selectors } from 'core/reducers/index';
-import { verifyCachedData } from 'core/utils';
-import { fetchMovie } from 'core/actions';
-
-const REQUIRED_FIELDS = ["tagline"];
-
 import api_dataV2 from 'views/utils/api_data.json';
-import { api_data } from 'views/pages/VideoProfile/data/api_data';
 import { comments } from 'views/pages/VideoProfile/data/api_data';
 
 
 function VideoProfile() {
-  const dispatch = useDispatch();
   const { videoId } = useParams();
-  // const isFetching = useSelector(state =>
-  //   selectors.selectIsFetchingVideo(state, videoId)
-  // );
-  // const video = useSelector(state => selectors.selectVideo(state, videoId));
-  //
-  // useEffect(() => {
-  //   dispatch(fetchVideo(videoId, REQUIRED_FIELDS));
-  // }, [videoId, dispatch]);
-
-  // const loading = isFetching || !verifyCachedData(video, REQUIRED_FIELDS);
-
-  // return (
-  //   <Profile
-  //     // loading={loading}
-  //     introduction={<VideoIntroduction videoId={videoId} url={api_data[5].url} />}
-  //     main={
-  //       <>
-  //
-  //
-  //
-  //         <VideoWrapper>
-  //           {api_data.map((item, index) =>
-  //               // <StyledVideoCard
-  //               // <VideoCard
-  //               // <VideoPlayer
-  //               // <CustomPlayer
-  //               // <CustomPlayerV2
-  //               // <CustomPlayerV3
-  //               <CustomPlayerV4
-  //               // <CustomBardPlayer
-  //               // <VideoPlayerV2
-  //                 url={item.url}
-  //                 key={index}
-  //                 // light={item.url}
-  //                 light={item.thumbNail}
-  //                 viewsCount={item.viewsCount}
-  //                 likesCount={item.likesCount}
-  //               />
-  //             )
-  //           }
-  //         </VideoWrapper>
-  //       </>
-  //     }
-  //
-  //   />
-  // );
-
-
-
-
-  const [videoSrc, setVideoSrc] = useState(null);
-  const [error, setError] = useState(null);
-  const [title, setTitle] = useState(null);
+  const [video, setVideo] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    if (!videoId) return;
     const fetchVideo = async () => {
       try {
-        const response = await axios.get(`http://localhost:8000/videos/media/call/${videoId}`);
-
-        console.log(response.data.dirname);
-        setVideoSrc(response.data.hls_file); // Assuming response.data.videoUrl contains the HLS URL (.m3u8)
-        setTitle(response.data.name); // Assuming response.data.videoUrl contains the HLS URL (.m3u8)
+        const res = await axios.get(`${LITLOOP_API_URL}/videos/${videoId}/`, { headers: authHeader() });
+        setVideo(res.data);
       } catch (e) {
-        console.log(e);
-        setError(error);
+        console.error(e);
+      } finally {
+        setLoading(false);
       }
     };
-
     fetchVideo();
-  }, []);
+  }, [videoId]);
 
+  if (loading) return <LayoutContainer><MainContent><p>Loading...</p></MainContent></LayoutContainer>;
+  if (!video) return <LayoutContainer><MainContent><p>Video not found.</p></MainContent></LayoutContainer>;
 
   return (
-    <>
     <LayoutContainer>
       <MainContent>
         <VideoIntroduction
           videoId={videoId}
-          // url={api_data[5].url}
-          url={videoSrc}
-
-          />
-        <h2>{title}</h2>
+          url={video.r2_url || video.gcs_url || video.url}
+        />
+        <h2>{video.title || video.name}</h2>
+        {video.description && <Description>{video.description}</Description>}
+        <MetaRow>
+          <span>{video.views_count || 0} views</span>
+          <span>{video.likes_count || 0} likes</span>
+        </MetaRow>
         <p>Comments</p>
         {comments.map((comment) => (
           <CommentV4 key={comment.id} comment={comment} />
         ))}
-
-        {/*<App/>*/}
-        {/*<RedditApp/>*/}
       </MainContent>
 
       <VideoWrapper>
         {api_dataV2.map((item, index) =>
-            // <StyledVideoCard
-            // <VideoCard
-            // <VideoPlayer
-            // <CustomPlayer
-            // <CustomPlayerV2
-            // <CustomPlayerV3
-            // <CustomPlayerV4
-            <RecommendedVideo
-            // <CustomBardPlayer
-            // <VideoPlayerV2
-              url={item.url}
-              key={index}
-              // light={item.url}
-              light={item.thumbNail}
-              viewsCount={item.viewsCount}
-              likesCount={item.likesCount}
-            />
-          )
-        }
+          <RecommendedVideo
+            url={item.url}
+            key={index}
+            light={item.thumbNail}
+            viewsCount={item.viewsCount}
+            likesCount={item.likesCount}
+          />
+        )}
       </VideoWrapper>
-
-      {/*<Section>*/}
-
-      {/*</Section>*/}
     </LayoutContainer>
-
-    </>
   );
-
-
 }
+
+
+const Description = styled.p`
+  color: var(--text);
+  opacity: 0.8;
+  margin: 8px 0;
+`;
+
+const MetaRow = styled.div`
+  display: flex;
+  gap: 16px;
+  color: var(--text);
+  opacity: 0.6;
+  font-size: 0.9rem;
+  margin-bottom: 16px;
+`;
 
 const LayoutContainer = styled.div`
   display: flex;
